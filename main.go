6316993780ad9c3bd2147284cb0b1b23e6e7293b
James Findley <jfindley@fastmail.com>
2015-07-26 19:09:27 +0100

    Move the rest of the core interfaces into core/

--- main.go
+++ main.go
@@ -2,22 +2,5 @@ package main
 
 import ()
 
-type Module interface {
-	Name() string
-	Start()
-	Wait()
-	Run(ResponseWriter, Input)
-}
-
-type ResponseWriter interface {
-	Success(bool)
-	TriggerCallbacks(bool)
-	Message(string, ...interface{})
-}
-
-type Input interface {
-	Data() map[string]interface{}
-}
-
 func main() {
 }
